fcbfabccf56371f0e987eb8d51862cab930fc3a0
LoFi-LiFe <newpmt88@gmail.com>
2021-09-16 13:33:47 +0900

    *차순 변경

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-	cout << "*****" << endl;
-	cout << "****" << endl;
-	cout << "***" << endl;
-	cout << "**" << endl;
 	cout << "*" << endl;
+	cout << "**" << endl;
+	cout << "***" << endl;
+	cout << "****" << endl;
+	cout << "*****" << endl;
 
 	return 0;
 }
